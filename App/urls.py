cdc00147ea3cdce440e4387a3a66aae3dbc89e1a
Amitesh Rai <abhiapor543@gmail.com>
2024-02-18 23:57:22 +0530

    Updated path

--- App/urls.py
+++ App/urls.py
@@ -5,7 +5,7 @@ from .views import signup, login, create_note, get_note, update_note
 urlpatterns = [
  path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
-    path('notes/create/', create_note, name='create_note'),
-    path('notes/<int:id>/', get_note, name='get_note'),
-    path('notes/<int:id>/', update_note, name='update_note'),
+    path('note/create/', create_note, name='create_note'),
+    path('note/<int:id>/', get_note, name='get_note'),
+    path('note/<int:id>/', update_note, name='update_note'),
 ]
